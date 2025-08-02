8a252ceb1666ecb4fcfc31cb9c95b278520a52b2
Bexruz1betmen <brawlacc1236@gmail.com>
2025-08-02 15:02:35 +0500

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,23 @@
+let gg = prompt ('Имя ')
+console.log( gg);
+
+let gk = prompt ('Отчество')
+console.log(gk);
+
+let ga = prompt ('Фамилия')
+console.log( ga);
+
+let gs = prompt ('нечинчи илсан укам')
+console.log( gs);
+
+let gd = prompt ('махала нма')
+console.log( gd);
+
+let wowa = confirm ('Вой запалку')
+
+alert(`нма гяп ${gg} л@@ 
+нма гяп ${gk} л@@ 
+нма гяп ${ga} л@@ 
+нма гяп ${gs} л@@ 
+нма гяп ${gd} л@@ `
+)
